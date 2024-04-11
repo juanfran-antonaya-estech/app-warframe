76c4de2e0f5cbb93d9e64a027879760e3c439de2
Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>
2024-04-11 11:23:25 +0200

    Layout de mainActivity Integrado
    Signed-off-by: Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    viewBinding{
+        enable = true
+    }
 }
 
 dependencies {
@@ -44,6 +48,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
