4b3dae35af9405cb423535c560eb2aadde8dcc7d
Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>
2024-04-16 11:49:44 +0200

    En camino de eventos
    Signed-off-by: Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>

--- app/src/main/java/com/juanfra/warframeinfoapi/model/WarfRepositorio.kt
+++ app/src/main/java/com/juanfra/warframeinfoapi/model/WarfRepositorio.kt
@@ -5,6 +5,7 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.juanfra.warframeinfoapi.model.data.defaultEndpoint.Alerta
 import com.juanfra.warframeinfoapi.model.data.defaultEndpoint.DefaultEndpoint
+import com.juanfra.warframeinfoapi.model.data.defaultEndpoint.Evento
 import com.juanfra.warframeinfoapi.model.data.defaultEndpoint.Noticia
 import com.juanfra.warframeinfoapi.model.retrofit.RetroFitService
 import com.juanfra.warframeinfoapi.model.retrofit.RetroWarf
